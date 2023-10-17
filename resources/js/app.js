bfbad5f9ca94bdd0061222979a968341a5ff2984
OthmanCharai <otmanchara3i0688@gmail.com>
2023-10-17 04:13:20 +0000

    setup vue router , entry point to vue app

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
-require('./bootstrap');
+require("./bootstrap");
+
+// Depandency goes here
+import { createApp } from "vue";
+import { createRouter, createWebHistory } from "vue-router";
+
+// Components go here
+import ProductIndex from "./components/products/index";
+
+// define varianbles & const
+const routes = [
+    {
+        path: "/",
+        component: ProductIndex,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp({})
+    .use(router)
+    .mount("#app");
